99ad034a65077d502ffa08bbfc9b5eedef963f88
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2023-09-25 18:28:14 +0200

    Delete D_Java/Projects/SpringBoot-Maven/ProfesoresACasa_Versions directory

--- D_Java/Projects/SpringBoot-Maven/ProfesoresACasa_Versions/1.0-OnlyWebsite/profesores_a_casa/src/main/java/es/codeurjc/dad/profesores_a_casa/repository/RankingRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.dad.profesores_a_casa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.codeurjc.dad.profesores_a_casa.model.Ranking;
-
-public interface RankingRepository extends JpaRepository<Ranking,Long>{
-    
-}
-
--- D_Java/Projects/SpringBoot-Maven/ProfesoresACasa_Versions/2.0-SecurityAndMailService/profesores_a_casa/src/main/java/es/codeurjc/dad/profesores_a_casa/repository/ReportRepository.java
+++
@@ -1,8 +0,0 @@
-package es.codeurjc.dad.profesores_a_casa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.codeurjc.dad.profesores_a_casa.model.Report;
-
-public interface ReportRepository extends JpaRepository<Report,Long>{
-    
-}
