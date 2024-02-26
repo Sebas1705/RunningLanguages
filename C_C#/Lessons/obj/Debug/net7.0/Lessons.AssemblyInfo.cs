146a5c28bdf5c973f7a86582c9b7148b674508c8
Sebas1705 <sebssgarcia502580@gmail.com>
2024-02-26 14:06:35 +0100

    HHH

--- C_C#/Lessons/obj/Debug/net7.0/Lessons.AssemblyInfo.cs
+++ C_C#/Lessons/obj/Debug/net7.0/Lessons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240a12640f278e83762628ddba82a474c9cf7c76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09b349cec56d971e3c4fef8940273fb53091caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- L_Gams/Lab1/LAB1_Ejercicio5.gms
+++ L_Gams/Lab1/LAB1_Ejercicio5.gms
@@ -15,12 +15,14 @@ beneficio
 restriccion_1
 restriccion_2
 restriccion_3
+restriccion_4
 ;
 
 beneficio..      z =e= 0.15 * x1 + 0.12 * x2 + 0.20 * x3;
-restriccion_1..  10 * x1 + 15 * x2 + 5 * x3 =l= 10;
-restriccion_2..  40 * x1 + 90 * x2 + 30 * x3 =g= 50;
-restriccion_3..  x1 =g= 0.4;
+restriccion_1..  x1 + x2 + x3 =e= 1;
+restriccion_2..  10 * x1 + 15 * x2 + 5 * x3 =l= 10;
+restriccion_3..  40 * x1 + 90 * x2 + 30 * x3 =g= 50;
+restriccion_4..  x1 =g= 0.4;
 ;
 
 Model Lab1_Ejercicio5 /all/;
